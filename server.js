b8b4b26c1980e11b305fffcf071760459a92026e
camarnath <camarnath@castlighthealth.com>
2020-07-15 21:08:33 +0530

    more colors

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-var path = require('path');
-var express = require('express');
+// "path" to prepare path
+// "express" server. Install it via npm
+const path = require('path');
+const express = require('express');
 
-var app = express();
+const app = express();
 
+// It serves the static files from "dist" path
 app.use(express.static(path.join(__dirname, 'dist')));
-app.set('port', process.env.PORT || 8080);
 
-var server = app.listen(app.get('port'), function() {
-  console.log('listening on port ', server.address().port);
-});
+// very important for heroku where port is assigned dynamically from its env else use default
+// app.set('port', process.env.PORT || 8080);
+const PORT = process.env.PORT || 8080;
+// const server = app.listen(app.get('port'), () => {
+//   console.log('listening on port ', server.address().port);
+// });
+app.listen(PORT);
