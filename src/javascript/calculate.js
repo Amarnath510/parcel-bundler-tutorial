cea5f65b1478bda834ae518bed631a1c212f8a60
camarnath <camarnath@castlighthealth.com>
2020-07-15 17:05:00 +0530

    included js file

---
+++ src/javascript/calculate.js
@@ -0,0 +1,9 @@
+const addTwoNumbers = (a, b) => {
+  return a + b;
+}
+
+const multiplyTwoNumbers = (a, b) => {
+  return a * b;
+}
+
+export { addTwoNumbers, multiplyTwoNumbers }
--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,3 +1,7 @@
 import '../sass/main.scss'
+import { addTwoNumbers, multiplyTwoNumbers } from './calculate.js'
 
 console.log('testing');
+
+console.log('Add two numbers ', addTwoNumbers(1, 2))
+console.log('Multiply two numbers ', multiplyTwoNumbers(10, 20))
